6cde3ed60360dc68f5d93dc993706ab7ef0be8b7
DanieLuan <danielluanlourencol@gmail.com>
2022-12-30 17:46:29 -0300

    start

--- src/App.js
+++ src/App.js
@@ -1,24 +1,35 @@
-import logo from './logo.svg';
-import './App.css';
+import {FiSearch} from 'react-icons/fi';
+import './styles.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="container">
+
+      <div className='containerInput'>
+        <h1 className="title">Buscador CEP</h1>
+
+        <input
+          type="text"
+          placeholder = "Insira o seu CEP"
+        />
+
+        <button className="buttonSearch">
+          <FiSearch size={25} color="#FFF"/>
+        </button>
+      </div>
+      
+      <main className='main'>
+        <h2>CEP 59149-193</h2>
+
+        <span>Rua José Ferreira de Lima</span>
+        <span>Emaús</span>
+        <span>Parnamirim</span>
+        <span>RN</span>
+      </main>
+
     </div>
+
+    
   );
 }
 
